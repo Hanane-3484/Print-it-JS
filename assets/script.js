b161ca7bc3f59a1a7f07834a630037d50a1e6c36
Hanane-3484 <Hanane.dahri3484@gmail.com>
2024-09-11 11:25:22 +0200

    finalisation

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,43 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+// Click et orientation des flèches du slide
+document.querySelector(".arrow_left").addEventListener("click", () => {
+    ChangeSlide(-1);
+    });
+
+document.querySelector(".arrow_right").addEventListener("click", () => {
+    ChangeSlide(1);
+    });
+    
+let i = 0;
+
+function ChangeSlide(sens) {
+        i = i + sens;
+        if (i > slides.length - 1)
+        i = 0 ;
+        if (i < 0)
+        i = slides.length - 1;
+
+//Chargement des images en fonction du click
+let image = document.querySelector(".banner-img");
+image.setAttribute("src", "./assets/images/slideshow/"+ slides[i].image);
+
+//Chargement des tagline des images en fonction du click
+document.querySelector("#banner p").innerHTML = slides[i].tagLine;
+
+//Chargement du bullet actif en fonction du click
+let bullet = document.querySelectorAll('.dot');
+bullet.forEach(dot => {
+dot.classList.remove('dot_selected');
+});
+bullet[i].classList.add('dot_selected');
+}
+
+let dots = document.querySelector("#banner .dots");
+for (let i = 0; i < slides.length; i++) {
+let dot = document.createElement('span');
+dot.classList.add('dot');
+dots.appendChild(dot);
+}
